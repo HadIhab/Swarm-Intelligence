9aabf833db89f1c2fb908d2a7387b11d75ebe53a
HadIhab <chocovani101@hotmail.fr>
2019-06-06 20:38:02 +0100

    Creation solve() method , first creation of the initial population ..

--- src/search_methodes/Genetic.java
+++ src/search_methodes/Genetic.java
@@ -1,5 +1,24 @@
 package search_methodes;
 
-public class Genetic {
+import models.Instance;
+import java.util.*;
 
+public class Genetic {
+	
+	private Instance instance;
+	private LinkedList<Integer> solution;
+	
+	public Genetic(Instance instance) {
+		this.instance=instance;
+	}
+
+	public void solve() {
+		/* Création d'une population initiale */
+		int pop_init_number=10;
+		for (int i=0;i<pop_init_number;i++) {
+			
+		}
+	
+	}
+	
 }
